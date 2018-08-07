366af0e4f65b8b39674fe09e40cd7b685f13fb75
g4rick <285699093@qq.com>
2018-08-07 23:18:48 +0800

    use eslint-config-airbnb-base

---
+++ index.js
@@ -0,0 +1,19 @@
+module.exports = {
+  parser: 'babel-eslint',
+
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+    mocha: true
+  },
+
+  globals: {
+    expect: true,
+    jest: true
+  },
+
+  extends: ['eslint-config-airbnb-base', 'plugin:react/recommended'],
+
+  rules: require('./rules')
+};
