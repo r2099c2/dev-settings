db0494fa7736acaafa7587ebb9c9010a116e40ec
g4rick <285699093@qq.com>
2018-08-24 18:19:09 +0800

    不使用末尾逗号

--- myRules.js
+++ myRules.js
@@ -1,4 +1,5 @@
 module.exports = {
   'react/jsx-filename-extension': [1, { 'extensions': ['.js', '.jsx'] }],
-  'react/prop-types': 0
+  'react/prop-types': 0,
+  "comma-dangle": ["error", "never"]
 };
